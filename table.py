78803d1253c72976d29f3a2bced92d4cc4ed83a0
OrrinJohnson <orrin24johnson@gmail.com>
2021-01-02 19:01:02 -0800

    added methods to clear the list after round

--- table.py
+++ table.py
@@ -1,3 +1,11 @@
 class Table:
     def __init__(self):
         self.cards = []
+
+    def clear(self):
+        for card in self.cards:
+            self.cards.remove(card)
+
+    def clear_all(self):
+        for i in range(len(self.cards)):
+            self.clear()
